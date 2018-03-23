5a63a9ea95a1fae33f766b1473f0a87ace554e91
cass_dev <cass_dev@tc-apple.ru>
2018-03-23 18:30:43 +0500

    started to work with exercise23_12

--- part23/exercise23_12/main.cpp
+++ part23/exercise23_12/main.cpp
@@ -4,6 +4,86 @@
 
 //------------------------------------------------------------------------------
 
+const vector<regex>& regex_tb()
+{
+    static vector<regex> vr {
+        regex{R"(([12]\d|0?[1-9]|3[01])[\.\\\s/\-]+(0?[1-9]|1[0-2])[\.\\\s/\-]+(\d{4}))"},
+        regex{R"((\d{4})[\.\\\s/]+(0?[1-9]|1[0-2])[\.\\\s/]+([12]\d|0?[1-9]|3[01]))"},
+        regex{R"((\d{4})[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+([12]\d|0?[1-9]|3[0-1]))"},
+        regex{R"(([12]\d|0?[1-9]|3[0-1])[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+(\d{4}))"}
+    };
+    return vr;
+}
+
+//------------------------------------------------------------------------------
+
+enum class Month {
+    jan=1, feb, mar, apr, may, jun,
+    jul, aug, sep, oct, nov, dec
+};
+
+//------------------------------------------------------------------------------
+
+const map<string, Month>& month_tb()
+{
+    static map<string, Month> vr {
+        {"jan", Month::jan}, {"Jan", Month::jan},
+        {"feb", Month::feb}, {"Feb", Month::feb},
+        {"mar", Month::mar}, {"Mar", Month::mar},
+        {"apr", Month::apr}, {"Apr", Month::apr},
+        {"may", Month::may}, {"May", Month::may},
+        {"jun", Month::jun}, {"Jun", Month::jun},
+        {"jul", Month::jul}, {"Jul", Month::jul},
+        {"aug", Month::aug}, {"Aug", Month::aug},
+        {"sep", Month::sep}, {"Sep", Month::sep},
+        {"oct", Month::oct}, {"Oct", Month::oct},
+        {"nov", Month::nov}, {"Nov", Month::nov},
+        {"dec", Month::dec}, {"Dec", Month::dec}
+    };
+    return vr;
+}
+
+//------------------------------------------------------------------------------
+
+bool is_year(const string& str)
+{
+    if (str.size() == 4)
+        return true;
+    return false;
+}
+
+//------------------------------------------------------------------------------
+
+void exercise23_12()
+{
+    ifstream in{"./file"};
+    if (!in) error("Нет файла");
+
+    ofstream out{"./file.out"};
+
+    const auto& regextb{regex_tb()};
+    int lineno{0};
+    for (string line; getline(in, line); ) {
+        ++lineno;
+        smatch matches;
+        for (const auto& r : regextb) {      // Рассматриваем все шаблоны
+            // Ищем первое совпадение
+            if (regex_search(line, matches, r))
+                cout << lineno << ": " << matches[0] << '\n';
+            else
+                continue;
+
+            // Ищем остальные совпадения
+            for (string suffix{matches.suffix().str()};
+                 regex_search(suffix, matches, r);
+                 suffix = matches.suffix().str())
+                cout << lineno << ": " << matches[0] << '\n';
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
 int main()
 try
 {
--- part23/exercise23_6/main.cpp
+++ part23/exercise23_6/main.cpp
@@ -7,10 +7,10 @@
 const vector<regex>& regex_tb()
 {
     static vector<regex> vr {
-        regex{R"(([0-2]?[1-9]|3[01])[\.\\\s/\-]+(0?[1-9]|1[0-2])[\.\\\s/\-]+(\d{4}))"},
-        regex{R"((\d{4})[\.\\\s/\-]+(0?[1-9]|1[0-2])[\.\\\s/\-]+([0-2]?[1-9]|3[01]))"},
-        regex{R"((\d{4})[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+([0-2]?[1-9]|3[0-1]))"},
-        regex{R"(([0-2]?[1-9]|3[0-1])[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+(\d{4}))"}
+        regex{R"(([12]\d|0?[1-9]|3[01])[\.\\\s/\-]+(0?[1-9]|1[0-2])[\.\\\s/\-]+(\d{4}))"},
+        regex{R"((\d{4})[\.\\\s/]+(0?[1-9]|1[0-2])[\.\\\s/]+([12]\d|0?[1-9]|3[01]))"},
+        regex{R"((\d{4})[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+([12]\d|0?[1-9]|3[0-1]))"},
+        regex{R"(([12]\d|0?[1-9]|3[0-1])[\.\\\s/\-]+([a-zA-Z]{3})[\.\\\s/\-]+(\d{4}))"}
     };
     return vr;
 }
@@ -19,19 +19,26 @@ const vector<regex>& regex_tb()
 
 void exercise23_6()
 {
-    ifstream in{"./file"};                   // Входной файл
-    if (!in) cerr << "Нет файла\n";
-    // cout << "шаблон: " << pat << '\n';
+    ifstream in{"./file"};
+    if (!in) error("Нет файла");
 
     const auto& regextb{regex_tb()};
-    int lineno{0};
-    for (string line; getline(in, line); ) { // Чтение строки
+    int lineno{0};    
+    for (string line; getline(in, line); ) {
         ++lineno;
         smatch matches;
-        for (const auto& r : regextb)
+        for (const auto& r : regextb) {      // Рассматриваем все шаблоны
+            // Ищем первое совпадение
             if (regex_search(line, matches, r))
-                // Полное соответствие
                 cout << lineno << ": " << matches[0] << '\n';
+            else
+                continue;
+            // Ищем остальные совпадения
+            for (string suffix{matches.suffix().str()};
+                 regex_search(suffix, matches, r);
+                 suffix = matches.suffix().str())
+                cout << lineno << ": " << matches[0] << '\n';
+        }
     }
 }
 
